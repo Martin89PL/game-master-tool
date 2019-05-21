a71d3751b5fcb1ae406a6a54fb6fecda6b5a47b6
Arkadiusz Nowakowski <a.nowakowski@devesoft.pl>
2019-05-21 08:56:53 +0200

    feat: add codeception unit tests

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php 
+class ExampleTest extends \Codeception\Test\Unit
+{
+    protected function _before()
+    {
+    }
+    protected function _after()
+    {
+    }
+    public function testSomeFeature()
+    {
+        $this->assertTrue(true); // :)
+    }
+}
