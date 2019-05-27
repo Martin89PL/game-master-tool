45f52cbdf7bfb3c595807a3b764005aa836770b0
Marcin <marcingladkowski@gmail.com>
2019-05-28 00:13:15 +0200

    (feat) use gameStan class to handle command prompt

--- src/prompt.php
+++ src/prompt.php
@@ -2,9 +2,11 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $input = '';
+$gameState = new GameState();
 GameMasterTool\Terminal::printWelcomeMessage('Put your names here');
 while ($input !== 'exit') {
     GameMasterTool\Terminal::printCommandPrompt();
     $input = rtrim(fgets(STDIN));
-    /* Process the input here... */
+    $result = $gameState->run($input);
+    echo $result;
 }
