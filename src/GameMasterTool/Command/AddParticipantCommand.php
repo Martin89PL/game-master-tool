e7f853485d209ad26f774563fdde472712e53638
Marcin <marcingladkowski@gmail.com>
2019-05-28 00:10:45 +0200

    (feat) create command to add participants

---
+++ src/GameMasterTool/Command/AddParticipantCommand.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace GameMasterTool\Command;
+
+class AddParticipantCommand implements CommandInterface
+{
+    public function execute(): void
+    {
+        // TODO: Implement execute() method.
+    }
+}
