f512c308a0d3fcdb79ad1c859e39d4b58f542057
Marcin <marcingladkowski@gmail.com>
2019-05-29 19:55:34 +0200

    (test) add test expected expection

--- tests/GameMasterTool/Factory/CommandFactoryTest.php
+++ tests/GameMasterTool/Factory/CommandFactoryTest.php
@@ -1,7 +1,8 @@
 <?php
 
-use GameMasterTool\Command\AddParticipantCommand;
+use GameMasterTool\Command\CreateParticipantCommand;
 use GameMasterTool\Factory\CommandFactory;
+use GameMasterTool\GameState;
 
 class CommandFactoryTest extends \Codeception\Test\Unit
 {
@@ -17,8 +18,24 @@ class CommandFactoryTest extends \Codeception\Test\Unit
     // tests
     public function testShoulReturnAddParticipantCommand()
     {
+        /** @var \PHPUnit\Framework\MockObject\MockObject | \GameMasterTool\Service\ParticipantService $participantService */
+        $participantService = self::createMock(\GameMasterTool\Service\ParticipantService::class);
+        $gameState = new GameState($participantService);
         $commmandPromptInput = 'add ParticipantName x PHY x MEN x VIT x LUC Skill1 Skill2';
-        $addParticipantCommand = CommandFactory::create($commmandPromptInput);
-        self::assertInstanceOf(AddParticipantCommand::class, $addParticipantCommand);
+        $addParticipantCommand = CommandFactory::create($gameState, $commmandPromptInput);
+        self::assertInstanceOf(CreateParticipantCommand::class, $addParticipantCommand);
     }
+
+    public function testShouldThrowExceptionWhenCommandIsBad()
+    {
+        self::expectException(InvalidArgumentException::class);
+
+        /** @var \PHPUnit\Framework\MockObject\MockObject | \GameMasterTool\Service\ParticipantService $participantService */
+        $participantService = self::createMock(\GameMasterTool\Service\ParticipantService::class);
+        $gameState = new GameState($participantService);
+        $commmandPromptInput = 'bad command';
+        CommandFactory::create($gameState, $commmandPromptInput);
+    }
+
+
 }
